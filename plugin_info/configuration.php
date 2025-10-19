5592972a0fd1a43340ac38d3d179a94b4babe8bf
Mathieu <34892335+OUARZA@users.noreply.github.com>
2025-10-19 21:47:17 +0200

    Randomize update time configuration and manage cron

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -21,39 +21,55 @@ if (!isConnect()) {
   include_file('desktop', '404', 'php');
   die();
 }
+
+require_once dirname(__FILE__) . '/../core/class/vigieau.class.php';
+
+$cronHour = config::byKey('cronConfHeure', 'vigieau', '');
+$cronMinute = config::byKey('cronConfMinute', 'vigieau', '');
+
+if ($cronHour === '' || $cronHour === null) {
+  $legacyHour = config::byKey('cronHeure', 'vigieau', '');
+  if ($legacyHour !== '' && $legacyHour !== null) {
+    $cronHour = (int) $legacyHour;
+  } else {
+    $cronHour = rand(0, 23);
+  }
+  config::save('cronConfHeure', $cronHour, 'vigieau');
+}
+
+if ($cronMinute === '' || $cronMinute === null) {
+  $cronMinute = rand(0, 59);
+  config::save('cronConfMinute', $cronMinute, 'vigieau');
+}
+
+vigieau::ensureDailyCron();
 ?>
 <form class="form-horizontal">
   <fieldset>
     <div class="form-group">
-      <label class="col-md-4 control-label">{{Heure de mise à jour}}
-<!--        <sup><i class="fas fa-question-circle tooltips" title="{{Heure à laquelle le plugin va chercher les informations}}"></i></sup>
-      </label>
-      <div class="col-md-1">
-        <input class="configKey form-control" data-l1key="cronHeure"/>
-      </div>
-    </div>
-  </fieldset>
-</form>
--->
+      <label class="col-md-4 control-label">
+        {{Heure de mise à jour}}
         <sup><i class="fas fa-question-circle tooltips" title="{{Heure à laquelle le plugin va chercher les informations}}"></i></sup>
       </label>
       <div class="col-md-1">
-      	<select class="configKey form-control" data-l1key="cronConfHeure">
-        	<?php
-            	for ($heure=0; $heure<24; $heure++) {
-                	echo '<option value="'.$heure.'">'.$heure.'h</option>';
-                }
-            ?>    
-		</select>
+        <select class="configKey form-control" data-l1key="cronConfHeure">
+          <?php
+          for ($hour = 0; $hour < 24; $hour++) {
+            $selected = ((int) $cronHour === $hour) ? ' selected' : '';
+            echo '<option value="' . $hour . '"' . $selected . '>' . str_pad($hour, 2, '0', STR_PAD_LEFT) . 'h</option>';
+          }
+          ?>
+        </select>
       </div>
       <div class="col-md-1">
-      	<select class="configKey form-control" data-l1key="cronConfMinute">
-        	<?php
-            	for ($minute=0; $minute<60; $minute++) {
-                	echo '<option value="'.$minute.'">'.$minute.'min</option>';
-                }
-            ?>    
-		     </select>
+        <select class="configKey form-control" data-l1key="cronConfMinute">
+          <?php
+          for ($minute = 0; $minute < 60; $minute++) {
+            $selected = ((int) $cronMinute === $minute) ? ' selected' : '';
+            echo '<option value="' . $minute . '"' . $selected . '>' . str_pad($minute, 2, '0', STR_PAD_LEFT) . 'min</option>';
+          }
+          ?>
+        </select>
       </div>
     </div>
   </fieldset>
