e24a5ce6cafe0352674bf673f7fb618bd7cfa95c
Mathieu <34892335+OUARZA@users.noreply.github.com>
2025-10-21 20:07:37 +0200

    Update vigieau.php

--- desktop/php/vigieau.php
+++ desktop/php/vigieau.php
@@ -192,11 +192,12 @@ $eqLogics = eqLogic::byType($plugin->getId());
                                         <select class="form-control" id="vigieauCommuneSelect" data-placeholder="<?php echo __('Sélectionnez une commune', __FILE__); ?>">
                                                 <option value=""><?php echo __('Sélectionnez une commune', __FILE__); ?></option>
                                         </select>
+                                        <span class="help-block"><a href="https://datanova.laposte.fr/datasets/laposte-hexasmal/full" target="_blank"><?php echo __('Liste des codes INSEE', __FILE__); ?></a></span>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label class="col-sm-4 control-label">{{Profil de consommation :}}</label>
-                                <div class="col-sm-5">
+								<label class="col-sm-4 control-label">{{Profil de consommation :}}</label>
+                                <div class="col-sm-3">
                                   <select class="form-control eqLogicAttr" data-l1key="configuration" data-l2key="typeInfo">
                                     <option value="particulier">{{Particulier}}</option>
                                     <option value="entreprise">{{Entreprise}}</option>
@@ -207,7 +208,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
                                 </div>
                             <div class="form-group">
                                 <label class="col-sm-4 control-label">{{Type d'eau consommé :}}</label>
-                                <div class="col-sm-5">
+                                <div class="col-sm-3">
                                   <select class="form-control eqLogicAttr" data-l1key="configuration" data-l2key="typeRestriction">
                                     <option value="aep">{{Du robinet}}</option>
                                     <option value="sup">{{D'un cours d'eau ou d'une rivière}}</option>
@@ -244,18 +245,13 @@ $eqLogics = eqLogic::byType($plugin->getId());
 						<div class="col-lg-6">
 							<legend><i class="fas fa-info"></i> {{Informations}}</legend>
 							
-                                <div class="form-group">
-                                        <label class="col-sm-3 control-label">{{Département :}}</label>
-                                        <span class="eqLogicAttr control-label" style="font-size:1em;cursor: default;" data-l1key="configuration" data-l2key="departement"></span>
+                            	<div class="form-group">
+									<label class="col-sm-3 control-label">{{Département :}}</label>
+                                	<span class="eqLogicAttr control-label" style="font-size:1em;cursor: default;" data-l1key="configuration" data-l2key="departement"></span>	
                                 </div>
                                 <div class="form-group">
-                                        <label class="col-sm-3 control-label">{{Commune :}}</label>
-                                        <span class="eqLogicAttr control-label" style="font-size:1em;cursor: default;" data-l1key="configuration" data-l2key="commune"></span>
-                                </div>
-                                <div class="form-group">
-                                        <label class="col-sm-3 control-label">{{Code INSEE :}}</label>
-                                        <span class="eqLogicAttr control-label" style="font-size:1em;cursor: default;" data-l1key="configuration" data-l2key="codeInseeCommune"></span>
-                                </div>
+                                	<label class="col-sm-3 control-label">{{Commune :}}</label>
+                                	<span class="eqLogicAttr control-label" style="font-size:1em;cursor: default;" data-l1key="configuration" data-l2key="commune"></span>												</div>
 						</div>
 					</fieldset>
 				</form>
