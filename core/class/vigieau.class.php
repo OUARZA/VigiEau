8799f4d05c63efb7ebcddc0b65891c1fa89a92b2
Mathieu <34892335+OUARZA@users.noreply.github.com>
2025-10-22 20:54:25 +0200

    Update vigieau.class.php

--- core/class/vigieau.class.php
+++ core/class/vigieau.class.php
@@ -122,12 +122,6 @@ class vigieau extends eqLogic {
 
   // Fonction exécutée automatiquement avant la création de l'équipement
   public function preInsert() {
-	if ($this->getIsEnable() != 1) {
-	$this->setIsEnable(1);
-	}
-	if ($this->getIsVisible() != 1) {
-	$this->setIsVisible(1);
-	}
   }
 
   // Fonction exécutée automatiquement après la création de l'équipement
